b9693e5fe36a3b5998c288d988ca49129f6bedd8
Priyanshu Sharma <serjoramos4444@gmail.com>
2024-12-02 21:54:28 +0545

    Add project files.

---
+++ Ecommerce_ASPDOTNET_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_ASPDOTNET_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
