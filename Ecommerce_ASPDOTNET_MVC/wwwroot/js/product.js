0a92cfa3960d1ad1a8ceba597d7218f10e9982ca
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-01-28 23:06:58 +0545

    json retrieve data

---
+++ Ecommerce_ASPDOTNET_MVC/wwwroot/js/product.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/product/getall',
+            type: 'GET',
+            datatype: 'json'
+        },
+        "columns": [
+            { data: 'title', title: 'Title' },
+            { data: 'isbn', title: 'ISBN' },
+            { data: 'listPrice', title: 'Price' },
+            { data: 'author', title: 'Author' },
+            { data: 'category.name', title: 'Category' }
+        ],
+        "responsive": true,
+        "autoWidth": false,
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "lengthMenu": [5, 10, 20, 50],
+        "language": {
+            "emptyTable": "No data available in table"
+        }
+    });
+}
