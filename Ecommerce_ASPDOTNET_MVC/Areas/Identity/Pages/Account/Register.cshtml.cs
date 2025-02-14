a88b5b4694c62492b8259680a667a678fc7b94a2
Priyanshu Sharma <serjoramos4444@gmail.com>
2025-02-14 16:19:34 +0545

    Enhance Manage Account

--- Ecommerce_ASPDOTNET_MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ecommerce_ASPDOTNET_MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -114,6 +114,19 @@ namespace Ecommerce_ASPDOTNET_MVC.Areas.Identity.Pages.Account
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
 
+            [Required]
+            public string? City { get; set; }
+            public string? StreetAddress { get; set; }
+
+            public string? State {  get; set; }
+
+            public string? PostalCode {  get; set; }
+            public string? PhoneNumber {  get; set; }
+
+            public string? Name {  get; set; }
+
+
+
         }
 
 
@@ -152,6 +165,14 @@ namespace Ecommerce_ASPDOTNET_MVC.Areas.Identity.Pages.Account
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
+                user.City = Input.City;
+                user.State = Input.State;
+                user.StreetAddress = Input.StreetAddress;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.PostalCode = Input.PostalCode;
+                user.Name = Input.Name;
+                
 
                 if (result.Succeeded)
                 {
